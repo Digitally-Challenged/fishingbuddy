bd295920eafa99c4580af854c582603887f7be60
Nick Coleman <154543891+Digitally-Challenged@users.noreply.github.com>
2025-12-13 06:35:05 -0600

    feat: import Special Elite and Lora font packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+// Fonts
+import '@fontsource/special-elite';
+import '@fontsource/lora/400.css';
+import '@fontsource/lora/500.css';
+import '@fontsource/lora/600.css';
+import '@fontsource/lora/700.css';
+
 import App from './App.tsx';
 import './index.css';
 
